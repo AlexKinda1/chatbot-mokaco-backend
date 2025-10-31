ab51893148730931afae86dd3396e945f9f9a451
AlexKinda1 <kindabenjaminalex@gmail.com>
2025-10-31 12:19:06 +0100

    Ajout de paramètres

--- src/params.py
+++ src/params.py
@@ -9,3 +9,18 @@ RAW_DATA_PATH = "data/raw"
 MANUALS_PATH = os.path.join(RAW_DATA_PATH, "manuals")
 FAQ_PATH = os.path.join(RAW_DATA_PATH, "faq")
 
+#Base de donnée vectorielle
+PERSIST_DIRECTORY = "data/vector_db"
+
+#Modelès de LLM
+OPEN_AI_LLM= 
+MIXTRAL_LLM=
+IBM_LLM=
+LLAMA_LLM= 
+
+#Paramétrage
+MAX_NEW_TOKENS= 
+TEMPERATURE=
+CHUNK_SIZE=
+CHUNK_OVERSIZE=
+
