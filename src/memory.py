e5588c9d4673e4c2214491422c6ac18dce3147a9
AlexKinda1 <kindabenjaminalex@gmail.com>
2025-11-04 23:04:33 +0100

    ajout de memoir

---
+++ src/memory.py
@@ -0,0 +1,26 @@
+docsearch = vector
+def qa():
+    history=[]
+    memory=ConversationBufferMemory(memory_key="chat_history", return_messages=False)
+    qa=ConversationalRetrievalChain.from_llm(
+                                           llm=llm_model,
+                                           retriever=docsearch.as_retriever(),
+                                           chain_type="stuff",
+                                           memory=memory,
+                                           get_chat_history=lambda h :h,
+                                           return_source_documents=False)
+    while True:
+
+        query = input("Question:")
+
+        if query.lower in ["quit", "exit"]:
+             print("Answer: Goodbye")
+             break
+        
+        result=qa({"question": query}, {"chat_history": history})
+
+        history.append((query, result["answer"]))
+
+        print("Answer:", result["answer"])
+
+qa()
