7749ad38b0327f470d2dbd0a4d4ec88bf1f7f15c
AlexKinda1 <kindabenjaminalex@gmail.com>
2025-11-12 22:51:06 +0100

    Memory modif 2

--- src/memory.py
+++ src/memory.py
@@ -1,8 +1,47 @@
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
+from ingest import load_documents_from_path, split_documents, initialize_embeddings, create_vector_store
+
+
+
+    logging.info("--- DÉMARRAGE DU SCRIPT D'INGESTION ---")
+    
+    # 1. Charger les manuels
+    manual_docs = load_documents_from_path(MANUALS_PATH, doc_type="manual")
+    
+    # 2. Charger la FAQ
+    faq_docs = load_documents_from_path(FAQ_PATH, doc_type="faq")
+    
+    all_documents = manual_docs + faq_docs
+    if not all_documents:
+        logging.error("Aucun document trouvé. Vérifiez vos dossiers /data/raw/manuals et /data/raw/faq.")
+        return
+
+    # 3. Découper tous les documents
+    all_chunks = split_documents(all_documents)
+    
+    # 4. Initialiser le modèle d'embedding (cela peut prendre du temps la 1ère fois)
+    embeddings_model = initialize_embeddings()
+    
+    # 5. Créer la base vectorielle
+    create_vector_store(all_chunks, embeddings_model)
+    
+    logging.info("--- SCRIPT D'INGESTION TERMINÉ ---")
 
 
 def qa():
+    #Chargement des documents    
+    documents=load_documents_from_path(mettre le bon path)
+    logging.info("Documents chargés.")
+
+    #Splitting des documents
+    chunks=split_documents(documents)
+    logging.info("Documents découpés en chunks.")
+
+    #Initialisation des embeddings
+    
+    
+    
     history=[]
     
     memory=ConversationBufferMemory(memory_key="chat_history", return_messages=False)
