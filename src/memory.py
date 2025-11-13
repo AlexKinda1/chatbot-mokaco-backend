ea4594e1df4d09e23c1e772f7c722130c19e6a1f
AlexKinda1 <kindabenjaminalex@gmail.com>
2025-11-13 23:46:13 +0100

    2

--- src/memory.py
+++ src/memory.py
@@ -1,34 +1,9 @@
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
+import logging
 from ingest import load_documents_from_path, split_documents, initialize_embeddings, create_vector_store
-
-
-
-    logging.info("--- DÉMARRAGE DU SCRIPT D'INGESTION ---")
     
-    # 1. Charger les manuels
-    manual_docs = load_documents_from_path(MANUALS_PATH, doc_type="manual")
     
-    # 2. Charger la FAQ
-    faq_docs = load_documents_from_path(FAQ_PATH, doc_type="faq")
-    
-    all_documents = manual_docs + faq_docs
-    if not all_documents:
-        logging.error("Aucun document trouvé. Vérifiez vos dossiers /data/raw/manuals et /data/raw/faq.")
-        return
-
-    # 3. Découper tous les documents
-    all_chunks = split_documents(all_documents)
-    
-    # 4. Initialiser le modèle d'embedding (cela peut prendre du temps la 1ère fois)
-    embeddings_model = initialize_embeddings()
-    
-    # 5. Créer la base vectorielle
-    create_vector_store(all_chunks, embeddings_model)
-    
-    logging.info("--- SCRIPT D'INGESTION TERMINÉ ---")
-
-
 def qa():
     #Chargement des documents    
     documents=load_documents_from_path(mettre le bon path)
@@ -39,8 +14,12 @@ def qa():
     logging.info("Documents découpés en chunks.")
 
     #Initialisation des embeddings
+    embeddings_model = initialize_embeddings()
     
+    #Créer la base vectorielle
+    create_vector_store(all_chunks, embeddings_model)
     
+    logging.info("--- INGESTION TERMINÉE ---")
     
     history=[]
     
