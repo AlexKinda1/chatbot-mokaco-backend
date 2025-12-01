5b5214234aa4377e4d04b64a3ac0a26e431fc701
AlexKinda1 <kindabenjaminalex@gmail.com>
2025-12-01 23:48:17 +0100

    params Up

--- src/params.py
+++ src/params.py
@@ -22,38 +22,35 @@ MIXTRAL_LLM =
 IBM_LLM =
 LLAMA_LLM =
 
+
+pinecone_api_key = pcsk_K34dn_JadARxwXqsKvMQy6CBKa2sneVkqZoS6xasE2Fd9vYPbCSqdBXg9GuHVpwJDji4y
+
+
 # Paramétrage
 MAX_NEW_TOKENS = 250
+MIN_NEW_TOKEN  = 50
 TEMPERATURE = 0.3
-TOP_P =
-TOP_K =
-CHUNK_SIZE = 50
-CHUNK_OVERSIZE =
+TOP_P = 0.9
+TOP_K = 25
+CHUNK_SIZE = 700
+CHUNK_OVERLAP = 100
 
-
-
-def define_template(model):
-
-    template = """
-    Tu es "MokaBot", un assistant technique expert pour les machines à café Mokaco. 
-    Tu es amical, précis et professionnel.
+template = """
+Tu es "MokaBot", un assistant technique expert pour les machines à café Mokaco. 
+Tu es amical, précis et professionnel.
     
-    Réponds à la question de l'utilisateur en te basant UNIQUEMENT sur le contexte suivant :
+Réponds à la question de l'utilisateur en te basant UNIQUEMENT sur le contexte suivant :
     
-    --- CONTEXTE ---
-    {context}
-    ---
+--- CONTEXTE ---
+{context}
+---
     
-    Question de l'utilisateur : {question}
+Question de l'utilisateur : {question}
     
-    Règles strictes :
-    1. Si le contexte ne contient pas la réponse, dis poliment : "Je suis désolé, je n'ai pas trouvé d'information précise sur ce sujet. Veuillez contacter l'assistance client"
-    2. Ne réponds PAS à des questions hors sujet (météo, histoire, etc.). Si la question n'a rien à voir avec Mokaco ou les machines à café, réponds : "Je suis spécialisé dans l'assistance pour les produits Mokaco. Je ne peux pas répondre à cette question."
-    3. Cite tes sources si possible, en mentionnant le nom du document (ex: "d'après le manuel machine_A.pdf").
+Règles strictes :
+1. Si le contexte ne contient pas la réponse, dis poliment : "Je suis désolé, je n'ai pas trouvé d'information précise sur ce sujet. Veuillez contacter l'assistance client"
+2. Ne réponds PAS à des questions hors sujet (météo, histoire, etc.). Si la question n'a rien à voir avec Mokaco ou les machines à café, réponds : "Je suis spécialisé dans l'assistance pour les produits Mokaco. Je ne peux pas répondre à cette question."
+3. Cite tes sources si possible, en mentionnant le nom du document (ex: "d'après le manuel machine_A.pdf").
     
-    Réponse :
-    """
-
-    prompt = ChatPromptTemplate.from_template(template)
-
-    logging.info("Template de prompt défini.")
+Réponse :
+"""
